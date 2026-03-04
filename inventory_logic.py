7f02c13da5fe2e969d35b360bdaced9249008fe8
abhiandabi2004-coder <abhiandabi2004@gmail.com>
2026-03-05 00:40:01 +0530

    Update inventory_logic.py

--- inventory_logic.py
+++ inventory_logic.py
@@ -1,20 +1,21 @@
-import pandas as pd
-
 def calculate_inventory_metrics(df):
 
     df["Daily_Sales"] = df["Sales_Last_30_Days"] / 30
 
-    df["Daily_Sales"] = df["Daily_Sales"].replace(0, 0.01)
+    df["Daily_Sales"] = df["Daily_Sales"].replace(0,0.01)
 
     df["Days_of_Stock"] = df["Current_Stock"] / df["Daily_Sales"]
 
     df["Inventory_Value"] = df["Current_Stock"] * df["Purchase_Cost"]
 
     def stock_status(days):
+
         if days < 7:
             return "Reorder"
+
         elif days <= 60:
             return "Normal"
+
         else:
             return "Overstock"
 
